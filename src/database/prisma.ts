84e46f745d8a4ca9ee35ffed5864792494fe6132
Thalisu <thalisondmcc@gmail.com>
2024-08-15 12:25:32 -0300

    refactor: remove mocked database

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,16 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { IUser } from "../interfaces/user.interface";
 
 const prisma = new PrismaClient();
-
-let id = 0;
-
-export const generateId = () => {
-  id++;
-  return id;
-};
-
-export const userDatabase: IUser[] = [];
 
 export default prisma;
 

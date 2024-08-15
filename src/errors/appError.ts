a77bc127af75ece2791f0f755451562bda10f0f0
Thalisu <thalisondmcc@gmail.com>
2024-08-15 15:23:45 -0300

    refactor: change to export default

--- src/errors/appError.ts
+++ src/errors/appError.ts
@@ -1,4 +1,4 @@
-export class AppError extends Error {
+class AppError extends Error {
   statusCode: number;
 
   constructor(message: string, statusCode = 400) {
@@ -6,3 +6,5 @@ export class AppError extends Error {
     this.statusCode = statusCode;
   }
 }
+
+export default AppError;

96ffc0975abee7e76574848ea4196c68f7ff3353
Thalisu <thalisondmcc@gmail.com>
2024-08-15 16:22:04 -0300

    feat: add prisma error handler in the error middleware

--- src/middleware/handleError.middleware.ts
+++ src/middleware/handleError.middleware.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from "express";
-import { AppError } from "../errors/appError";
+import AppError from "../errors/appError";
 import { JsonWebTokenError } from "jsonwebtoken";
 import { ZodError } from "zod";
+import { PrismaError } from "../errors/prismaError";
 
 export class handleErrors {
   static execute(
@@ -16,6 +17,10 @@ export class handleErrors {
       return res.status(403).json({ message: error.message });
     } else if (error instanceof ZodError) {
       return res.status(400).json({ message: error.issues });
+    } else if (PrismaError.isKnownError(error)) {
+      return res
+        .status(PrismaError.statusCode)
+        .json({ message: error.message });
     } else {
       console.error(error);
       return res.status(500).json({ message: "Internal server error." });
